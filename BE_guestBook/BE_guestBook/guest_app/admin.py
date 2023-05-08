8ff785281420171df7dc5ce0849d3fc7a2c824f5
literalllyme1 <kde7891@naver.com>
2023-05-08 20:33:47 +0900

    six

---
+++ BE_guestBook/BE_guestBook/guest_app/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import GuestBook
+# Register your models here.
+class GuestBookAdmin(admin.ModelAdmin):   
+    list_display = ('title','author','content','datetime')
+    
+admin.site.register(GuestBook, GuestBookAdmin)
+
+
