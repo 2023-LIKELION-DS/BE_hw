32fbf01d8fb059b8b1f5b0cb53b72a33ad180765
7beunseo <kimes0403@gmail.com>
2023-05-08 20:29:01 +0900

    [0508] 백엔드 6차세션

---
+++ BE_guestBook/guest_app/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from .models import GuestBook
+
+def index(request):
+    books=GuestBook.objects.all()
+    return render(request, "index.html", {'books':books})
