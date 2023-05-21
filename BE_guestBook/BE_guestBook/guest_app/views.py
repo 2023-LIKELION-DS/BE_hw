18707e48d7b18e21ba3ca9f960cfae8db45db9d3
76MINJI <mnxxj1128@naver.com>
2023-05-22 07:34:22 +0900

    [0522] Django 과제 제출

--- BE_guestBook/BE_guestBook/guest_app/views.py
+++ BE_guestBook/BE_guestBook/guest_app/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 from .models import GuestBook
+from django.views.generic import ListView
+# from django.view.list import ListView
 # Create your views here.
 def index(request):
     books = GuestBook.objects.all()
     return render(request, "index.html", {'books' : books})
+
+class GuestList(ListView):
+    model = GuestBook
+    # template_name = 'class_view.html'
+    ordering = ['-datetime']
+    
+def guestapp_list(request):
+    return render(request, 'guestbook_list.html')
