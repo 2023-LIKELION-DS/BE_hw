cbe49278a0021f1b075d8b9b5d8b1f0991dfbf99
daye02 <innostus002@naver.com>
2023-05-22 18:11:54 +0900

    [0522] django 실습

--- BE_guestBook/BE_guestBook/urls.py
+++ BE_guestBook/BE_guestBook/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from guest_app import views
-from guest_app.views import index
+from guest_app.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- BE_guestBook/guest_app/guest_app/urls.py
+++ BE_guestBook/guest_app/guest_app/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from guest_app import views
+from guest_app.views import *
 
 app_name = 'guest_app'
 
 
 urlpatterns=[
-        path('', views.GuestList.as_view, name = 'guestbook-list'),
+        path('', GuestList.as_view, name = 'guestbook-list'),
 ]
